d350485497f8cfd7d97d2e07e5838a0b3c5336e9
rsarao <richardsarao@me.com>
2016-10-17 18:19:30 -0700

    save before bootstrap. added home and about and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'pages/home' #this is the path of folders in the omrails folder. pages is in views folder and home is the home.html.erb file
+
+  root 'pages#home' #this makes the home.html.erb file the home page at localhost 3000
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'about' => 'pages#about'  #how to create a new route
+
+ get 'contact' => 'pages#contact' 
 end
