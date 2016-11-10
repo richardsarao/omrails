3643439839d39c8761eaaf870c38cd313ed7ef2e
rsarao <richardsarao@me.com>
2016-11-09 17:29:46 -0800

    added images to tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -61,6 +61,6 @@ class TweetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tweet_params
-      params.require(:tweet).permit(:user_id, :content)
+      params.require(:tweet).permit(:content, :image)
     end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,6 +6,9 @@ class Tweet < ApplicationRecord
   validates :user, presence: true
 
   acts_as_votable
+
+  has_attached_file :image  #vid 69
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
 
 
